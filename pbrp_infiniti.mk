77db192fc70615f7ea19bb184c9c7da0852a2a80
Travis82 <travismills82@gmail.com>
2026-09-08 09:03:33 -0500

    Pitchblack Recovery Oneplus 15
    Hello new recovery

---
+++ pbrp_infiniti.mk
@@ -0,0 +1,22 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/oplus/infiniti
+
+# Inherit from device.mk configuration
+$(call inherit-product, $(DEVICE_PATH)/device.mk)
+
+## Device identifier
+PRODUCT_DEVICE  := infiniti
+PRODUCT_NAME    := pbrp_infiniti
+PRODUCT_BRAND   := oplus
+TARGET_OTA_ASSERT_DEVICE := PLK110,OP611FL1,OP60FFL1,CPH2745,CPH2747,CPH2749
+
+# Theme
+TW_STATUS_ICONS_ALIGN   := center
+TW_CUSTOM_CLOCK_POS := 65
+TW_CUSTOM_CPU_POS := 240
+TW_CUSTOM_BATTERY_POS := 790
